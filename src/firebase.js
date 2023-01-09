9792327a10f0d63627f83afab85766471b43f8f9
OutcastR97 <sadman.rashiq.dbz@gmail.com>
2023-01-09 23:28:29 +0600

    website-design

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDiF_hKsK0gf-TCAKSb5JmF-iGqtihuaLc",
+    authDomain: "personal-ecommerce-website.firebaseapp.com",
+    projectId: "personal-ecommerce-website",
+    storageBucket: "personal-ecommerce-website.appspot.com",
+    messagingSenderId: "1051431433974",
+    appId: "1:1051431433974:web:23c18149f75363bcf85ed7",
+    measurementId: "G-C976829VYP"
+};
